9b85d37e974eae148a2b94d7cf0231b3ba9b4071
sarpsenturk <sarpsenturk38@gmail.com>
2026-07-02 21:04:06 +0300

    Remove platform layer, use SDL directly in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include "chip8.hpp"
-#include "platform.hpp"
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_log.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL_render.h>
 
 #include <chrono>
 #include <string>
@@ -21,8 +21,27 @@ int main(int argc, const char** argv)
     const auto scale = std::stoi(argv[1]);
     const char* rom = argv[2];
 
-    // Initialize SDL and platform layer
-    Platform platform("Chip8", Chip8::display_width, Chip8::display_height, scale);
+    // Initialize SDL
+    SDL_Init(SDL_INIT_VIDEO);
+
+    // Create window
+    SDL_Window* window = SDL_CreateWindow(
+        "Chip8",
+        Chip8::display_width * scale,
+        Chip8::display_height * scale,
+        0);
+
+    // Create renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+
+    // Create chip8 framebuffer
+    SDL_Texture* texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_RGBA8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        Chip8::display_width,
+        Chip8::display_height);
+    SDL_SetTextureScaleMode(texture, SDL_SCALEMODE_NEAREST);
 
     // Initialize interpreter
     Chip8 chip8;
@@ -35,12 +54,70 @@ int main(int argc, const char** argv)
     double cpu_accumulator = 0.0;
     double timer_accumulator = 0.0;
     auto last = std::chrono::high_resolution_clock::now();
-    while (!platform.should_quit()) {
+    while (true) {
         auto now = std::chrono::high_resolution_clock::now();
         double dt = std::chrono::duration<double>(now - last).count();
         last = now;
 
-        platform.update(chip8.keys());
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_EVENT_QUIT:
+                    goto exit;
+                case SDL_EVENT_KEY_DOWN:
+                case SDL_EVENT_KEY_UP:
+                    switch (event.key.key) {
+                        case SDLK_X:
+                            chip8.keys()[0] = event.key.down;
+                            break;
+                        case SDLK_1:
+                            chip8.keys()[1] = event.key.down;
+                            break;
+                        case SDLK_2:
+                            chip8.keys()[2] = event.key.down;
+                            break;
+                        case SDLK_3:
+                            chip8.keys()[3] = event.key.down;
+                            break;
+                        case SDLK_Q:
+                            chip8.keys()[4] = event.key.down;
+                            break;
+                        case SDLK_W:
+                            chip8.keys()[5] = event.key.down;
+                            break;
+                        case SDLK_E:
+                            chip8.keys()[6] = event.key.down;
+                            break;
+                        case SDLK_A:
+                            chip8.keys()[7] = event.key.down;
+                            break;
+                        case SDLK_S:
+                            chip8.keys()[8] = event.key.down;
+                            break;
+                        case SDLK_D:
+                            chip8.keys()[9] = event.key.down;
+                            break;
+                        case SDLK_Z:
+                            chip8.keys()[0xa] = event.key.down;
+                            break;
+                        case SDLK_C:
+                            chip8.keys()[0xb] = event.key.down;
+                            break;
+                        case SDLK_4:
+                            chip8.keys()[0xc] = event.key.down;
+                            break;
+                        case SDLK_R:
+                            chip8.keys()[0xd] = event.key.down;
+                            break;
+                        case SDLK_F:
+                            chip8.keys()[0xe] = event.key.down;
+                            break;
+                        case SDLK_V:
+                            chip8.keys()[0xf] = event.key.down;
+                            break;
+                    }
+            }
+        }
 
         cpu_accumulator += dt * cpu_hz;
         timer_accumulator += dt * timer_hz;
@@ -54,8 +131,16 @@ int main(int argc, const char** argv)
             timer_accumulator -= 1.0;
         }
 
-        platform.render(chip8.pixels(), chip8.display_pitch);
+        SDL_UpdateTexture(texture, nullptr, chip8.pixels(), Chip8::display_pitch);
+        SDL_RenderClear(renderer);
+        SDL_RenderTexture(renderer, texture, nullptr, nullptr);
+        SDL_RenderPresent(renderer);
     }
+
+    // Destroy window & shutdown SDL
+exit:
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
